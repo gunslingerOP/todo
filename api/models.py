d6ec97a38c4037b292d9f42fd5a9d73a3288a261
gunslingerOP <yahyahassan72@personal.com>
2022-02-05 17:50:29 +0300

    First draft

---
+++ api/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class OnlyActiveManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(is_active=True)   
+    
+    
+     
+class Task(models.Model):
+    title = models.TextField("Title")
+    creationDate =  models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    active= OnlyActiveManager
+
+    def __str__(self):
+        return self.name
+    class Meta:
+         ordering = ['creationDate']
+    
